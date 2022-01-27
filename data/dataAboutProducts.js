5be068e199e9c13789c3a145cfbda2ae55981e3b
PrKsushka <ksushkaprot@gmail.com>
2022-01-27 22:46:10 +0300

    add initial server logic

---
+++ data/dataAboutProducts.js
@@ -0,0 +1,15 @@
+let products=[
+    {
+        "displayName": "Cyberpank 2077",
+        "price": "60$",
+    },
+    {
+        "displayName": "SpongeBob SquarePants: Battle for Bikini Bottom – Rehydrated",
+        "price": "40$",
+    },
+    {
+        "displayName": "God Of War",
+        "price": "50$",
+    }
+];
+module.exports=products;
