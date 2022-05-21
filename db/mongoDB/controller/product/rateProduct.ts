b5e35b87577290acb29fe86f5d79b49d7af217dc
PrKsushka <ksushkaprot@gmail.com>
2022-05-21 16:52:31 +0300

    check rate

--- db/mongoDB/controller/product/rateProduct.ts
+++ db/mongoDB/controller/product/rateProduct.ts
@@ -9,62 +9,67 @@ const rateProductWithMongoDB = async (req: Request | any, res: Response) => {
     const { rate } = req.body;
     const { id } = req.params;
 
-    const findProduct = await Product.find({
-      _id: new ObjectId(id),
-      'ratings.userId': new ObjectId(req.user.id),
-    }).lean<Array<ProductsTypes>>();
-    if (findProduct[0]) {
+    if (Number(rate) >= 1 && Number(rate) <= 10) {
+      const findProduct = await Product.find({
+        _id: new ObjectId(id),
+        'ratings.userId': new ObjectId(req.user.id),
+      }).lean<Array<ProductsTypes>>();
+
+      if (findProduct[0]) {
+        await Product.updateOne(
+          {
+            _id: id,
+            'ratings.userId': new ObjectId(req.user.id),
+          },
+          {
+            $set: {
+              'ratings.$.userId': new ObjectId(req.user.id),
+              'ratings.$.rating': Number(rate),
+            },
+          }
+        );
+      } else {
+        await Product.updateOne(
+          {
+            _id: id,
+          },
+          {
+            $push: {
+              ratings: {
+                userId: new ObjectId(req.user.id),
+                rating: Number(rate),
+              },
+            },
+          }
+        );
+      }
+      const totalRating = await Product.aggregate([
+        {
+          $match: {
+            _id: new ObjectId(id),
+          },
+        },
+        {
+          $group: {
+            _id: '$temp',
+            totalRating: {
+              $sum: { $sum: '$ratings.rating' },
+            },
+          },
+        },
+      ]);
       await Product.updateOne(
         {
           _id: id,
           'ratings.userId': new ObjectId(req.user.id),
         },
         {
-          $set: {
-            'ratings.$.userId': new ObjectId(req.user.id),
-            'ratings.$.rating': Number(rate),
-          },
+          totalRating: totalRating[0].totalRating,
         }
       );
     } else {
-      await Product.updateOne(
-        {
-          _id: id,
-        },
-        {
-          $push: {
-            ratings: {
-              userId: new ObjectId(req.user.id),
-              rating: Number(rate),
-            },
-          },
-        }
-      );
+      return res.status(StatusCodes.BAD_REQUEST).send({ message: 'Rate should be 1-10' });
     }
-    const totalRating = await Product.aggregate([
-      {
-        $match: {
-          _id: new ObjectId(id),
-        },
-      },
-      {
-        $group: {
-          _id: '$temp',
-          totalRating: {
-            $sum: { $sum: '$ratings.rating' },
-          },
-        },
-      },
-    ]);
-    await Product.updateOne(
-      {
-        _id: id,
-        'ratings.userId': new ObjectId(req.user.id),
-      },
-      {
-        totalRating: totalRating[0].totalRating,
-      }
-    );
     res.status(StatusCodes.OK).json({ message: 'product rated successfully' });
   } catch (e: any) {
     res.status(e.statusCode).json({ message: e.message });
--- db/postgres/controller/product/rateProduct.ts
+++ db/postgres/controller/product/rateProduct.ts
@@ -10,34 +10,39 @@ const rateProduct = async (req: Request | any, res: Response) => {
     const { id } = req.params;
     const userRatings = await getRepository(UserRatings).createQueryBuilder('user_ratings');
 
-    const findIfRated = await userRatings
-      .where('user_ratings."productId"=:productId', { productId: Number(id) })
-      .andWhere('user_ratings."userId"=:userId', { userId: Number(req.user.id) })
-      .getOne();
-    if (findIfRated) {
-      await userRatings
-        .update(UserRatings)
-        .set({ rating: Number(rate) })
-        .where('user_ratings."userId"=:id', { id: req.user.id })
-        .andWhere('user_ratings."productId"=:id', { id })
-        .execute();
-    } else {
-      await userRatings
-        .insert()
-        .into(UserRatings)
-        .values({ user: req.user.id, product: id, rating: Number(rate) })
-        .execute();
-    }
-    const products = await getRepository(Product).createQueryBuilder('product');
-    const currentProduct = await products.where('product.id=:id', { id }).getOne();
+    if (Number(rate) >= 1 && Number(rate) <= 10) {
+      const findIfRated = await userRatings
+        .where('user_ratings."productId"=:productId', { productId: Number(id) })
+        .andWhere('user_ratings."userId"=:userId', { userId: Number(req.user.id) })
+        .getOne();
 
-    if (currentProduct) {
-      const sum = await getRepository(UserRatings).createQueryBuilder().select('SUM(rating)', 'sum').where('"productId"=:id', { id }).getRawOne();
-      await products
-        .update(Product)
-        .set({ totalRating: Number(sum.sum) })
-        .where('product.id=:id', { id })
-        .execute();
+      if (findIfRated) {
+        await userRatings
+          .update(UserRatings)
+          .set({ rating: Number(rate) })
+          .where('user_ratings."userId"=:id', { id: req.user.id })
+          .andWhere('user_ratings."productId"=:id', { id })
+          .execute();
+      } else {
+        await userRatings
+          .insert()
+          .into(UserRatings)
+          .values({ user: req.user.id, product: id, rating: Number(rate) })
+          .execute();
+      }
+
+      const products = await getRepository(Product).createQueryBuilder('product');
+      const currentProduct = await products.where('product.id=:id', { id }).getOne();
+      if (currentProduct) {
+        const sum = await getRepository(UserRatings).createQueryBuilder().select('SUM(rating)', 'sum').where('"productId"=:id', { id }).getRawOne();
+        await products
+          .update(Product)
+          .set({ totalRating: Number(sum.sum) })
+          .where('product.id=:id', { id })
+          .execute();
+      }
+    } else {
+      return res.status(StatusCodes.BAD_REQUEST).send({ message: 'Rate should be 1-10' });
     }
     res.status(StatusCodes.OK).json({ message: 'product successfully rated' });
   } catch (e: any) {
